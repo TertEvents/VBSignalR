222eff8031fbf8ab50ad910f9ec6749219f0a533
Bolaji Opemipo <micbol95@yahoo.com>
2016-11-28 18:03:23 +0100

    All Folders and Classes

---
+++ App_Code/HUB/ChatHub.vb
@@ -0,0 +1,26 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports Microsoft.AspNet.SignalR
+Imports Microsoft.AspNet.SignalR.Hubs
+Imports Microsoft.AspNet.SignalR.Transports
+Imports Microsoft.AspNet.SignalR.Infrastructure
+Imports System.Threading.Tasks
+Imports System.Data
+Imports System.Linq
+Imports JSON.Namespace
+Imports System.Text
+Imports System.Web
+Imports System.Web.Script.Serialization
+Imports System.Globalization
+
+Namespace signalr
+    Public Class ChatHub
+        Inherits Hub
+
+        Public Sub hub_send_message(name As String, message As String)
+            Clients.All.broadcastMessage(name, message)
+        End Sub
+
+    End Class
+
+End Namespace
---
+++ App_Code/HUB/STARTUP.vb
@@ -0,0 +1,15 @@
+﻿Imports Microsoft.Owin
+Imports Owin
+Imports Microsoft.AspNet.SignalR
+<Assembly: OwinStartup(GetType(signalr.Startup))>
+
+Namespace signalr
+    Public Class Startup
+        Public Sub Configuration(app As IAppBuilder)
+            ' Any connection or hub wire up and configuration should go here
+            Dim hubConfiguration = New HubConfiguration
+            hubConfiguration.EnableDetailedErrors = True
+            app.MapSignalR(hubConfiguration)
+        End Sub
+    End Class
+End Namespace
